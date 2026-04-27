d7b54afcc88e35729a74477acec38b2687f5bfe9
sobirov10101 <sobirovamir771@gmail.com>
2026-04-27 16:35:16 +0500

    salom

---
+++ github/github2/index.js
@@ -0,0 +1,7 @@
+// masala1
+const rasm = document.querySelector('img')
+rasm.setAttribute("width","100")
+// masala 2
+const link = document.querySelector('a')
+link.setAttribute("href","https://github.com")
+link.getAttribute("hello")
