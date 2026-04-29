5a3d8e39f972a6f92d1fc1531c82cb287e0077b5
sobirov10101 <sobirovamir771@gmail.com>
2026-04-29 16:44:07 +0500

    add

--- dars/index.js
+++ dars/index.js
@@ -1,10 +1,9 @@
-// masala 1
-const car = {
-    name: "BMW",
-    colar: "blue",
-    sey: function(){
-        console.log(`bu mashinani nomi ${this.name}`);
-    }
-}
-car.sey()
-// masala 2
+const listitem = document.querySelectorAll('li')
+listitem.forEach((iteam) => {
+
+    iteam.addEventListener('click', (iten) => {
+        iten.target.style.textDecoration = 'line-through'
+        iten.target.style.opacity = '0.8'
+    })
+
+})
