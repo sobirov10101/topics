7d289ca0eb1470682315dc525d4ba609e3a4b1e0
sobirov10101 <sobirovamir771@gmail.com>
2026-04-30 20:42:15 +0500

    masala birrr

--- index.js
+++ index.js
@@ -4,6 +4,24 @@ listitem.forEach((iteam) => {
     iteam.addEventListener('click', (iten) => {
         iten.target.style.textDecoration = 'line-through'
         iten.target.style.opacity = '0.8'
+        iten.target.remove()
     })
 
+})
+
+// bugun ozim organgan narsalarim_____________________________________________________1
+const ul = document.querySelector('ul')
+const button = document.querySelector('button')
+
+button.addEventListener('click', () => {
+    const li = document.createElement('li')
+    li.textContent = ('hello world')
+    ul.appendChild(li)
+
+})
+const all = document.querySelectorAll('li')
+all.forEach((ham) =>{
+  ham.addEventListener(('click'), (ham) =>{
+    ham.remove()
+  })
 })
